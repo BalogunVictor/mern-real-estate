4c6ac2fcdcab28ccc408699e7e5df8eed6d5eede
BalogunVictor <balogunmoyinoluwav@gmail.com>
2024-04-12 16:26:12 +0100

    complete sign up page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-import User from "../models/user.model";
+import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res, next) => {
--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -19,9 +20,8 @@ app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+// app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
