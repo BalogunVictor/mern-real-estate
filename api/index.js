c2a985bc6190896f71cc2d08ddf2b85c78c8ec25
BalogunVictor <balogunmoyinoluwav@gmail.com>
2024-05-05 18:08:13 +0100

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
 import listingRouter from "./routes/listing.route.js";
+import path from "path";
 
 dotenv.config();
 
@@ -25,10 +26,15 @@ app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
 
-// app.use("/api/user", userRouter);
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
