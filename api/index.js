d4755152e17dfef57bda5a77bad43d799edd58f6
BalogunVictor <balogunmoyinoluwav@gmail.com>
2024-05-06 07:35:26 +0100

    fixed bug

--- api/index.js
+++ api/index.js
@@ -5,7 +5,8 @@ import mongoose from "mongoose";
 import authRouter from "./routes/auth.route.js";
 import userRouter from "./routes/user.route.js";
 import listingRouter from "./routes/listing.route.js";
-import path from "path";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
 dotenv.config();
 
@@ -17,6 +18,9 @@ mongoose
   .catch(() => {
     console.log("Failed to connect to MongoDB");
   });
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 app.use(express.json());
@@ -30,10 +34,10 @@ app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(join(__dirname, "client", "dist", "index.html"));
 });
 
 app.use((err, req, res, next) => {
