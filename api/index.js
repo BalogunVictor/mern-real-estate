5b94a8259956e05009a4290071e5f35172ae3d3b
BalogunVictor <balogunmoyinoluwav@gmail.com>
2024-05-06 11:54:01 +0100

    fixed bug

--- api/index.js
+++ api/index.js
@@ -1,25 +1,28 @@
-import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
-import authRouter from "./routes/auth.route.js";
+import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
+import cookieParser from "cookie-parser";
 import path from "path";
-
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGODB)
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log("Connected to MongoDB!");
   })
-  .catch(() => {
-    console.log("Failed to connect to MongoDB");
+  .catch((err) => {
+    console.log(err);
   });
 
+const __dirname = path.resolve();
+
 const app = express();
+
 app.use(express.json());
+
 app.use(cookieParser());
 
 app.listen(3000, () => {
